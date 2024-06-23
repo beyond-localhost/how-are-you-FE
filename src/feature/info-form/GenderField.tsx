e3df13caf3423a071a01554b2f5cf80a1746d690
realzu <jinhh501@naver.com>
2024-06-23 23:16:19 +0900

    style: 성별 입력폼 스타일 추가

--- src/feature/info-form/GenderField.tsx
+++ src/feature/info-form/GenderField.tsx
@@ -1,6 +1,18 @@
-import { GENDER } from '@/constants/form.ts';
+import { GENDER, SELECT_TITLE_TEXT } from '@/constants/form.ts';
 import React from 'react';
 import { Gender, setCurInfoByKeyParam } from '@type/infoFormType.ts';
+import { InputTitle } from '@feature/info-form/styles/InfoForm.style.tsx';
+import {
+    GenderInputRadio,
+    GenderInputIconBox,
+    GenderInputWrapper,
+    GenderSelection
+} from '@feature/info-form/styles/GenderField.style.tsx';
+import { TextLabel } from '@components/text/Text.tsx';
+import { GenderMaleIcon } from '@components/icons/GenderMaleIcon.tsx';
+import { GenderFemaleIcon } from '@components/icons/GenderFemaleIcon.tsx';
+import { XIcon } from '@components/icons/XIcon.tsx';
+import { mauve } from '@/tokens/color.ts';
 
 interface GenderFormProp extends setCurInfoByKeyParam {
     gender: Gender;
@@ -13,24 +25,36 @@ const GenderField: React.FC<GenderFormProp> = ({ setCurInfoByKey, gender }) => {
 
     return (
         <>
-            <p>성별</p>
+            <InputTitle>{'성별' + SELECT_TITLE_TEXT}</InputTitle>
 
-            <div>
+            <GenderInputWrapper>
                 {GENDER.map(item => {
                     const { id, label } = item;
+                    const isChecked = gender === id;
                     return (
-                        <div key={id}>
-                            <input
+                        <GenderSelection key={id}>
+                            <GenderInputRadio
                                 type="radio"
                                 id={id}
-                                checked={gender === id}
+                                checked={isChecked}
                                 onChange={handleGenderCheck}
                             />
-                            <label htmlFor={id}>{label}</label>
-                        </div>
+                            <GenderInputIconBox htmlFor={id}>
+                                {id === 'male' ? (
+                                    <GenderMaleIcon isChecked={isChecked} />
+                                ) : id === 'female' ? (
+                                    <GenderFemaleIcon isChecked={isChecked} />
+                                ) : (
+                                    <XIcon isChecked={isChecked} />
+                                )}
+                            </GenderInputIconBox>
+                            <TextLabel htmlFor={id} size={3} weight={'bold'} color={mauve['12']}>
+                                {label}
+                            </TextLabel>
+                        </GenderSelection>
                     );
                 })}
-            </div>
+            </GenderInputWrapper>
         </>
     );
 };
