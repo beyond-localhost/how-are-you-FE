d89e7953a40de2447051f84813b0aba99b0afc22
steven semyung oh <semyung.dev@gmail.com>
2024-07-03 21:23:08 +0900

    feat: add header layout

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,13 +1,9 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { Container } from '@styles/Common.style.tsx';
 
 function Root() {
-    const location = useLocation();
-
     return (
         <Container>
-            {!['/', '/info-form'].includes(location.pathname) && <h1>How are you?</h1>}
-
             <Outlet />
         </Container>
     );
